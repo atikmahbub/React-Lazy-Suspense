24b98f4f929d54ba0772b829841bd7ab93f26d25
atikmahbub <atikmahbub100@gmail.com>
2020-04-03 20:18:42 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Route,
+} from "react-router-dom";
+const Nav = React.lazy(() => (import('./components/Nav')))
+const Home = React.lazy(() => (import('./components/Home')))
+const About = React.lazy(() => (import('./components/About')))
+
+const Loading = () =>{
+  return(
+    <div>Loading Nav...</div>
+  )
+}
+
+const LoadingPage = () =>{
+  return(
+    <div>Loading Pages...</div>
+  )
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <React.Suspense fallback = {<Loading/>}>
+        <Nav/>
+      </React.Suspense>
+
+    <React.Suspense fallback={<LoadingPage/>}>
+      <Router>
+        <Route exact path="/home"> <Home/> </Route>
+        <Route exact path="/about"> <About/> </Route>
+      </Router>
+    </React.Suspense>
     </div>
   );
 }
---
+++ src/components/Nav.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Nav() {
+    return (
+        <nav>
+            <a href="/home">Home Page</a> | <a href="/about">About Page</a>
+        </nav>
+    )
+}
+
+export default Nav
